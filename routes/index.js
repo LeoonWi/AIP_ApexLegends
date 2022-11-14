53b3925839c2bb8c35fd22e750071d0c2d8aff7b
LeoonWi <redwind090@gmail.com>
2022-11-14 22:29:59 +0300

    add templates

--- routes/index.js
+++ routes/index.js
@@ -3,22 +3,37 @@ var router = express.Router();
 
 /* Домашняя страница */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Apex Legends' });
+  res.render('index', {
+    title: "Apex Legends",
+    picture: "images/apex-legends-team.png"
+  });
 });
 
 /* Страница Рэйф */
 router.get('/wraith', function(req, res, next) {
-  res.send("<h1>Страница Рэйф</h1>")
+  res.render('legend', {
+    title: "Рэйф",
+    picture: "images/wraith.png",
+    desc: "Боец-вихрь, способный проводить смертоносные атаки, а также манипулировать пространством-временем, создавая разломы в ткани реальности."
+  })
 })
 
 /* Страница Октейна */
 router.get('/octane', function(req, res, next) {
-  res.send("<h1>Страница Октейна</h1>")
+  res.render('legend', {
+    title: "Октейн",
+    picture: "images/octane.png",
+    desc: "Скоростной сорвиголова, чьи умения включают в себя регенерацию здоровья, ускорение и установку катапульты. Неоновые трубки и цилиндр, прикрепленный к бедру Октейна, а также металлические ноги указывают на то, что Октейн — киборг."
+  })
 })
 
 /* Страница Крипто */
 router.get('/crypto', function(req, res, next) {
-  res.send("<h1>Страница Крипто</h1>")
+  res.render('legend', {
+    title: "Крипто",
+    picture: "images/crypto.png",
+    desc: "Эксперт по разведке. Этот гениальный хакер и мастер шифрования использует свои воздушные дроны для слежки за противниками на арене Apex, оставаясь в тени."
+  })
 })
 
 module.exports = router;
