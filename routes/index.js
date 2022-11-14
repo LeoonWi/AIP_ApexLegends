063a2c02eece6867bf9b135edd4f4d8364af0b62
LeoonWi <redwind090@gmail.com>
2022-11-14 22:02:44 +0300

    add png && 3 page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* Домашняя страница */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Apex Legends' });
 });
+
+/* Страница Рэйф */
+router.get('/wraith', function(req, res, next) {
+  res.send("<h1>Страница Рэйф</h1>")
+})
+
+/* Страница Октейна */
+router.get('/octane', function(req, res, next) {
+  res.send("<h1>Страница Октейна</h1>")
+})
+
+/* Страница Крипто */
+router.get('/crypto', function(req, res, next) {
+  res.send("<h1>Страница Крипто</h1>")
+})
 
 module.exports = router;
