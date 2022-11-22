4f3c6799579f6067b92d37b99ea6a30d6869960c
LeoonWi <redwind090@gmail.com>
2022-11-22 22:28:27 +0300

    update mongooseTasks.js

--- mongooseTasks.js
+++ mongooseTasks.js
@@ -1,14 +1,15 @@
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://127.0.0.1:27017/test')
 
-var Legend = mongoose.model('Legend', {name: String})
+var schema = mongoose.Schema({name: String})
+
+schema.methods.phrase = function() {
+    console.log(this.get("name") + " поздоровалась.")
+}
+
+var Legend = mongoose.model('Legend', schema)
 var Wraith = new Legend({name: 'Рэйф'})
 Wraith.save(function(err)
 {
-    if(err) {
-        console.log(err)
-    }
-    else {
-        console.log('APEXLEGENDS')
-    }
+    Wraith.phrase()
 })
