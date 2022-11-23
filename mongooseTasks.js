f032f60c7fbf223d7548874cee380a25c8808df2
LeoonWi <redwind090@gmail.com>
2022-11-23 18:21:19 +0300

    add models & update mongooseTasks.js

--- mongooseTasks.js
+++ mongooseTasks.js
@@ -1,15 +1,13 @@
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://127.0.0.1:27017/test')
+var Legend = require("./models/legend.js").Legend
 
-var schema = mongoose.Schema({name: String})
+var legend = new Legend({
+    title: "Рэйф",
+    nick: "wraith"
+})
 
-schema.methods.phrase = function() {
-    console.log(this.get("name") + " поздоровалась.")
-}
-
-var Legend = mongoose.model('Legend', schema)
-var Wraith = new Legend({name: 'Рэйф'})
-Wraith.save(function(err)
-{
-    Wraith.phrase()
+console.log(legend)
+legend.save(function(err, legend, affected){
+    console.log(legend.title)
 })
