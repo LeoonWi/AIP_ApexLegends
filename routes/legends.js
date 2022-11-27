c16722753922a4981319565958fcbf85b27f053f
LeoonWi <redwind090@gmail.com>
2022-11-27 19:34:12 +0300

    add request from db

--- routes/legends.js
+++ routes/legends.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const { Legend } = require('../models/legend');
 var router = express.Router();
 
 /* GET users listing. */
@@ -8,7 +9,15 @@ router.get('/', function(req, res, next) {
 
 /* Страница легенд */
 router.get('/:nick', function(req, res, next) {
-    res.send(req.params.nick);
+  Legend.findOne({nick:req.params.nick}, function(err, legend) {
+    if (err) return next(err)
+    if (!legend) return next(new Error ("Нет такой легенды"))
+    res.render('legend', {
+      title: legend.title,
+      picture: legend.avatar,
+      desc: legend.desc
+    })
+  })
 });
 
 module.exports = router;
