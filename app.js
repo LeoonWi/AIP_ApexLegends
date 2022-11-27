68ce1008f7cabc310664cc48bb486ff4e72f847b
LeoonWi <redwind090@gmail.com>
2022-11-27 15:54:07 +0300

    update

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/apexlegends')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var legendsRouter = require('./routes/legends');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/legends', legendsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
