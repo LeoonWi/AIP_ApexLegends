f5689b9eb468e3b36facf99d9becda5bbd6f998d
LeoonWi <redwind090@gmail.com>
2023-01-11 01:24:23 +0300

    Сделал создание сессии

--- app.js
+++ app.js
@@ -3,17 +3,29 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/apexlegends')
+//var mongoose = require('mongoose');
+//mongoose.connect('mongodb://127.0.0.1:27017/apexlegends')
+var mysql2 = require('mysql2/promise');
 var session = require('express-session')
-var MongoStore = require('connect-mongo');(session);
-var Legend = require("./models/legend").Legend
+//var MongoStore = require('connect-mongo');(session);
+var MySQLStore = require('express-mysql-session')(session);
+//var Legend = require("./models/legend").Legend
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var legendsRouter = require('./routes/legends');
 
 var app = express();
+
+var options = {
+    host: '127.0.0.1',
+    port: '3306',
+    user: 'root',
+    password: '2003',
+    database: 'apexlegends'
+};
+var connection = mysql2.createPool(options)
+var sessionStore = new MySQLStore(options, connection);
 
 // view engine setup
 app.engine('ejs', require('ejs-locals'));
@@ -25,7 +37,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session (
+app.use(express.static(path.join(__dirname, 'bower_components')));
+
+/* app.use(session (
     {
         secret: "ApexLegends",
         cookie: {maxAge: 60 * 1000},
@@ -33,13 +47,29 @@ app.use(session (
         saveUninitialized: true,
         store: MongoStore.create({mongoUrl: 'mongodb://127.0.0.1:27017/apexlegends'})
     }
-))
+)) */
+
+app.use(session (
+    {
+        secret: 'ApexLegends',
+        key: 'sid',
+        store: sessionStore,
+        resave: true,
+        saveUninitialized: true,
+        cookie: {
+            path: '/',
+            httpOnly: true,
+            maxAge: 60*1000
+        }
+    }
+));
+
 app.use(function(req, res, next) {
     req.session.counter = req.session.counter +1 || 1
     next()
 })
 
-app.use(function(req, res, next) {
+/* app.use(function(req, res, next) {
     res.locals.nav = []
 
     Legend.find({}, {_id: 0, title: 1, nick: 1}, function(err, result) {
@@ -47,7 +77,7 @@ app.use(function(req, res, next) {
         res.locals.nav = result
         next()
     })
-})
+}) */
 
 app.use(require("./middleware/createMenu.js"))
 app.use(require("./middleware/createUser.js"))
