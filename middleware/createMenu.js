93fc264646ed0b436905b5cb7a8c7b7bd0399ff7
LeoonWi <redwind090@gmail.com>
2023-01-10 22:44:34 +0300

    Что-то поменял... Сам не понял что

--- middleware/createMenu.js
+++ middleware/createMenu.js
@@ -3,7 +3,7 @@ var Legend = require("../models/legend").Legend
 module.exports = function(req, res, next) {
     res.locals.nav = []
 
-    Legend.find({}, {_id: 0, title: 1, nick: 1}, function(err, result) {
+    Legend.find(null, {_id: 0, title: 1, nick: 1}, function(err, result) {
         if(err) throw err
         res.locals.nav = result
         next()
